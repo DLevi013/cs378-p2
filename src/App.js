c40520836f40345f3065d0f559b54d0a8dd2d58d
DLevi013 <danielsujaylevi@utexas.edu>
2025-02-05 19:23:08 -0600

    made purple

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import MenuItem from './components/MenuItem';
 
 
-import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"></link>
+// import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></link>
+// {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"></link> */}
 
 // Menu data. An array of objects where each object represents a menu item. Each menu item has an id, title, description, image name, and price.
 // You can use the image name to get the image from the images folder.
